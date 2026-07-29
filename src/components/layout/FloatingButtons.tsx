104887c3dc70b96bc37e4fa535f9fb562d55c683
Adhi Varma <adhi290406@gmail.com>
2026-07-29 21:08:11 +0530

    Website updates

--- src/components/layout/FloatingButtons.tsx
+++ src/components/layout/FloatingButtons.tsx
@@ -76,7 +76,6 @@ export default function FloatingButtons() {
         initial={{ opacity: 0, x: 100 }}
         animate={{ opacity: 1, x: 0 }}
         transition={{ delay: 0.8 }}
-        className="hidden md:block"
       >
         <Link
           to="/contact"
